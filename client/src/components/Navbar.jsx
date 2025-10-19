745b6ca0a7e7034b5c9a3e328608af0055a4a7d8
Karthik P <padiyil2004@gmail.com>
2025-10-19 14:26:16 +0530

    completed the navbar frontend

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,32 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {assets} from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
+import { AppContext } from '../context/AppContext.jsx'
 
 const Navbar = () => {
   const navigate = useNavigate();
-  const [user,setUser] = useState(null);
+  const {user} = useContext(AppContext);
   return (
     <div className='flex items-center justify-between py-4'>
       <img src={assets.logo} alt="logo" className='w-28 cursor-pointer' onClick={() => navigate('/')}/>
       <div>
-        {user ? <div>
-
+        {user ? <div className='flex items-center gap-2'>
+          <button className='flex items-center gap-2 bg-blue-100 px-4 py-1.5 rounded-full hover:scale-105 transition-all duration-700'>
+            <img src={assets.credit_star} alt="credit" />
+            <p className='text-xs text-gray-600'>Credits Left : 50</p>
+          </button>
+          <p className='text-gray-600 pl-4'>Hi, Karthik</p>
+          <div className='group relative' >
+            <img src={assets.profile_icon} alt="profile" className='w-10 drop-shadow'/>
+            <div className='absolute hidden group-hover:block text-black rounded pt-4'>
+              <ul className='list-none m-0 p-2 bg-white rounded-md  text-sm ring-2 ring-blue-200'>
+                <li className='py-1 px-2 cursor-pointer'>LogOut</li>
+              </ul>
+            </div>
+          </div>
         </div> : <div className='flex items-center gap-8'>
         <p className='cursor-pointer' onClick={() => navigate('/buy')}> Pricing </p>
-        <button className='bg-zinc-800 text-white px-7 py-2 rounded-2xl'> Login </button>
+        <button className='bg-zinc-800 text-white px-7 py-2 rounded-2xl cursor-pointer'> Login </button>
         </div>}
       </div>
     </div>
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,18 @@
+import { createContext,useState } from "react";
+
+export const AppContext = createContext();
+
+const AppProvider = (props) => {
+  const [user,setUser] = useState(null);
+  
+  const value = {
+    user,setUser
+  }
+  return (
+    <AppContext.Provider value={value}>
+      {props.children}
+    </AppContext.Provider>
+  )
+}
+
+export default AppProvider;
